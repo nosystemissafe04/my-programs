b456247b105274c9b95dabb45aa714dd5c603f5d
nosystemissafe04 <99750087+nosystemissafe04@users.noreply.github.com>
2022-02-15 23:43:09 +0530

    Add files via upload

---
+++ checkingdivisibilitywithconditionaloperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int x,y,z;
+  printf("lets create an program to identify which variable is divisible or not (!~):) \n");
+  printf("enter the value of x:");
+  scanf("%d",&x);
+  printf("enter the value of y :");
+  scanf("%d",&y);
+  x%y==0?printf("the number is divisible by x:%d\n",y),z=(x/y),printf("x is %d times divided by y ",z):(printf("the number is not divisibile by y:%d\n",x));
+/*  z=(x/y);
+  printf("x is %d times divided by y ",z);*/
+}
