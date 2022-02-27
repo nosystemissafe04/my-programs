2e2c7ffc299793cb6106346282632b7b8d651643
nosystemissafe04 <99750087+nosystemissafe04@users.noreply.github.com>
2022-02-27 16:01:31 +0530

    Add files via upload

---
+++ displayingvariables.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+ int x ;
+ float y;
+ char a=' ';
+ double w;
+ long int c;
+ short int e;
+// unsighned u;
+ printf("int =%d\n",&x);
+ printf("float =%f\n",&y);
+ printf("char =%c\n",&a);
+ printf("double=%lf\n",&w);
+ printf("long int=%ld\n",&c);
+ printf("short int =%d\n",&e);
+// printf("int =%u\n",u);
+
+
+}
