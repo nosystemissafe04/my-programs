0fd6a8bfd9806484635a828a3c6b80722059cdf5
nosystemissafe04 <99750087+nosystemissafe04@users.noreply.github.com>
2022-03-02 16:41:42 +0530

    Add files via upload

---
+++ ali10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main()
+{
+int x;
+printf("enter the number :");
+scanf("%d",&x);
+printf("%d note(s) of 100",b=x/100);
+a=x-100*b;
+printf("%d note(s) of 50",c=a/50);
+d=a-50*c;
+printf("%d note(s) of 20",e=d/20);
+f=d-
+printf("%d note(s) of 10");
+printf("%d note(s) of 5");
+printf("%d coin(s) of 2");
+printf("%d coin(s) of 1");
+
+
+
+
+
+
+}
+
